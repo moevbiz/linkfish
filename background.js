d35350bc8b584da925e7f36527f22977032d2c8f
Bruno Mokross <bruno.mokross@gmail.com>
2020-02-03 18:57:01 +0100

    init

---
+++ background.js
@@ -0,0 +1,75 @@
+let importedBookmarks = {
+  'Tech News': [
+    {
+      'name': 'Awwwards',
+      'url': 'https://awwwards.com'
+    },
+    {
+      'name': 'Chrome',
+      'url': 'https://google.com'
+    }
+  ],
+  'Design News': [
+    {
+      'name': 'Pina',
+      'url': 'https://pinavienna.eu'
+    }
+  ]
+}
+
+let appName = 'Linkfish';
+
+function init() {
+  let hasFolder = false;
+  let folderId = '';
+
+  chrome.bookmarks.getTree(function(itemTree){
+    itemTree.forEach(function(item){
+      processNode(item);
+      if(hasFolder) return;
+    });
+    
+    // add app folder if it doesn't exist
+    if(!hasFolder){
+      console.log(itemTree);
+      createLfFolder();
+    }
+  });
+
+  function processNode(node) {
+    // recursively process child nodes
+    if (node.children) {
+      node.children.forEach(function(child) { 
+        processNode(child);
+        if(hasFolder) return;
+      });
+    }
+
+    // if app folder already exists, use it :~) 
+    if (node.title == appName) { 
+      hasFolder = true;
+      folderId = node.id;
+      console.log(appName + ' folder already exists: ID is ' + folderId);
+      chrome.storage.local.set({lfId: folderId}, function() {
+        console.log(appName + ' folder ID is set to ' + folderId);
+      });
+      return;
+    }
+  }
+}
+
+function createLfFolder() {
+  chrome.bookmarks.create({
+    title: appName,
+    url: null
+  }, onBookmarkAdded)
+}
+
+chrome.runtime.onInstalled.addListener(function() {
+  init();
+  console.log('Extension installed');
+});
+
+function onBookmarkAdded(bookmarkItem) {
+  console.log("Bookmark addded added with ID: " + bookmarkItem.id, bookmarkItem.title);
+}
